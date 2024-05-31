d4a352040bfbfeadcb4b64e597f14b04f382a8ff
G.J.M. Kleinlein <g.j.m.kleinlein@umail.leidenuniv.nl>
2024-05-31 23:26:51 +0200

    test

--- servo-test.py
+++ servo-test.py
@@ -4,21 +4,21 @@ from time import sleep   # Imports sleep (aka wait or pause) into the program
 from signal import pause
 
 # Set up pins for Servos
-servo_a = Servo(18)  # BCM equivalent of physical pin 12
-# servo_b = Servo(12)  # BCM equivalent of physical pin 32
+# servo_a = Servo(18)  # BCM equivalent of physical pin 12
+servo_b = Servo(12)  # BCM equivalent of physical pin 32
 # servo_c = Servo(13)  # BCM equivalent of physical pin 33
 # servo_d = Servo(19)  # BCM equivalent of physical pin 35
 
 try:
     while True:  # This will keep the servo sweep running until the process is interrupted
         # Move the servos back and forth
-        servo_a.value = 0.2
-        # servo_b.value = 0.2
+        # servo_a.value = 0.2
+        servo_b.value = 0.12
         # servo_c.value = 0.2
         # servo_d.value = 0.2
         sleep(1)
-        servo_a.value = 0.0
-        # servo_b.value = 0.0
+        # servo_a.value = 0.0
+        servo_b.value = 0.0
         # servo_c.value = 0.4
         # servo_d.value = 0.4
         sleep(1)
@@ -30,6 +30,7 @@ finally:
     # Disable all servos
     # for servo in [servo_a, servo_b, servo_c, servo_d]:
     #     servo.detach()
-    servo_a.detach()
+    # servo_a.detach()
+    servo_b.detach()
 
 pause()  # Wait for an interrupt signal (like Ctrl+C)
