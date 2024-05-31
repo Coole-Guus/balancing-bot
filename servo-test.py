2a4d33d6d1ec880ba402b9768475e27415be2e59
G.J.M. Kleinlein <g.j.m.kleinlein@umail.leidenuniv.nl>
2024-05-31 23:46:05 +0200

    test

--- servo-test.py
+++ servo-test.py
@@ -1,26 +1,28 @@
 # Set up libraries and overall settings
-from gpiozero import Servo
+import pigpio
 from time import sleep   # Imports sleep (aka wait or pause) into the program
-from signal import pause
 
 # Set up pins for Servos
-# servo_a = Servo(18)  # BCM equivalent of physical pin 12
-servo_b = Servo(12)  # BCM equivalent of physical pin 32
-# servo_c = Servo(13)  # BCM equivalent of physical pin 33
-# servo_d = Servo(19)  # BCM equivalent of physical pin 35
+servo_a = 18  # BCM equivalent of physical pin 12
+servo_b = 12  # BCM equivalent of physical pin 32
+servo_c = 13  # BCM equivalent of physical pin 33
+servo_d = 19  # BCM equivalent of physical pin 35
+
+# Create a pigpio object
+pi = pigpio.pi()
 
 try:
     while True:  # This will keep the servo sweep running until the process is interrupted
         # Move the servos back and forth
-        # servo_a.value = 0.2
-        servo_b.value = 0.12
-        # servo_c.value = 0.2
-        # servo_d.value = 0.2
+        pi.set_servo_pulsewidth(servo_a, 1000)  # Move the servo to its minimum position
+        pi.set_servo_pulsewidth(servo_b, 2000)  # Move the servo to its maximum position
+        pi.set_servo_pulsewidth(servo_c, 1000)  # Move the servo to its minimum position
+        pi.set_servo_pulsewidth(servo_d, 2000)  # Move the servo to its maximum position
         sleep(1)
-        # servo_a.value = 0.0
-        servo_b.value = 0.0
-        # servo_c.value = 0.4
-        # servo_d.value = 0.4
+        pi.set_servo_pulsewidth(servo_a, 1500)  # Move the servo to its middle position
+        pi.set_servo_pulsewidth(servo_b, 1500)  # Move the servo to its middle position
+        pi.set_servo_pulsewidth(servo_c, 1500)  # Move the servo to its middle position
+        pi.set_servo_pulsewidth(servo_d, 1500)  # Move the servo to its middle position
         sleep(1)
         
 except KeyboardInterrupt:  # This will catch the interrupt and move to the cleanup code
@@ -28,9 +30,8 @@ except KeyboardInterrupt:  # This will catch the interrupt and move to the clean
 
 finally:
     # Disable all servos
-    # for servo in [servo_a, servo_b, servo_c, servo_d]:
-    #     servo.detach()
-    # servo_a.detach()
-    servo_b.detach()
-
-pause()  # Wait for an interrupt signal (like Ctrl+C)
+    pi.set_servo_pulsewidth(servo_a, 0)
+    pi.set_servo_pulsewidth(servo_b, 0)
+    pi.set_servo_pulsewidth(servo_c, 0)
+    pi.set_servo_pulsewidth(servo_d, 0)
+    pi.stop()
