a4e9ee33797f6e86977c2c92b605caa12be52586
G.J.M. Kleinlein <g.j.m.kleinlein@umail.leidenuniv.nl>
2024-06-18 20:33:27 +0200

    testing

--- leg-ik.py
+++ leg-ik.py
@@ -8,41 +8,41 @@ LeftUpper = 11
 RightLower = 14
 RightUpper = 15
 
-def servo1():
-    for a in range(80,100):
-        kit.servo[10].angle = 90
-        sleep(0.008)
+# def servo1():
+#     for a in range(80,100):
+#         kit.servo[10].angle = 90
+#         sleep(0.008)
         
-    for a in range(100,80,-1):
-        kit.servo[10].angle = a
-        sleep(0.008)
+#     for a in range(100,80,-1):
+#         kit.servo[10].angle = a
+#         sleep(0.008)
         
-def servo2():
-    for a in range(80,100):
-        kit.servo[11].angle = 90
-        sleep(0.008)
+# def servo2():
+#     for a in range(80,100):
+#         kit.servo[11].angle = 90
+#         sleep(0.008)
         
-    # for a in range(100,80,-1):
-    #     kit.servo[11].angle = a
-    #     sleep(0.008)
+#     # for a in range(100,80,-1):
+#     #     kit.servo[11].angle = a
+#     #     sleep(0.008)
         
-def servo3():
-    # for a in range(80,100):
-        kit.servo[14].angle = 90
-    #     sleep(0.008)
+# def servo3():
+#     # for a in range(80,100):
+#         kit.servo[14].angle = 90
+#     #     sleep(0.008)
         
-    # for a in range(100,80,-1):
-    #     kit.servo[14].angle = a
-    #     sleep(0.008)
+#     # for a in range(100,80,-1):
+#     #     kit.servo[14].angle = a
+#     #     sleep(0.008)
         
-def servo4():
-    # for a in range(80,100):
-        kit.servo[15].angle = 90
-    #     sleep(0.008)
+# def servo4():
+#     # for a in range(80,100):
+#         kit.servo[15].angle = 90
+#     #     sleep(0.008)
         
-    # for a in range(100,80,-1):
-    #     kit.servo[15].angle = a
-    #     sleep(0.008)
+#     # for a in range(100,80,-1):
+#     #     kit.servo[15].angle = a
+#     #     sleep(0.008)
 
 
 
@@ -156,7 +156,7 @@ def setServo(servo: int, angle: float):
 
 min = 70
 max = 120
-precision = 100
+precision = 10
 timeQuantum = 0.0001
 
 def changeHeight(newVal: float, oldVal: float):
