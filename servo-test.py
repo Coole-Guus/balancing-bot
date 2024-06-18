ac68f84704961f33c4b66cae2d40f3a982eb8744
G.J.M. Kleinlein <g.j.m.kleinlein@umail.leidenuniv.nl>
2024-06-18 16:44:32 +0200

    testing

--- servo-test.py
+++ servo-test.py
@@ -1,37 +1,47 @@
-# Set up libraries and overall settings
-import pigpio
-from time import sleep   # Imports sleep (aka wait or pause) into the program
+from adafruit_servokit import ServoKit
+from time import sleep
+kit = ServoKit(channels=16)
 
-# Set up pins for Servos
-servo_a = 18  # BCM equivalent of physical pin 12
-servo_b = 12  # BCM equivalent of physical pin 32
-servo_c = 13  # BCM equivalent of physical pin 33
-servo_d = 19  # BCM equivalent of physical pin 35
 
-# Create a pigpio object
-pi = pigpio.pi()
-
-try:
-    while True:  # This will keep the servo sweep running until the process is interrupted
-        # Move the servos back and forth
-        pi.set_servo_pulsewidth(servo_a, 1000)  # Move the servo to its minimum position
-        pi.set_servo_pulsewidth(servo_b, 2000)  # Move the servo to its maximum position
-        pi.set_servo_pulsewidth(servo_c, 1000)  # Move the servo to its minimum position
-        pi.set_servo_pulsewidth(servo_d, 2000)  # Move the servo to its maximum position
-        sleep(1)
-        pi.set_servo_pulsewidth(servo_a, 1500)  # Move the servo to its middle position
-        pi.set_servo_pulsewidth(servo_b, 1500)  # Move the servo to its middle position
-        pi.set_servo_pulsewidth(servo_c, 1500)  # Move the servo to its middle position
-        pi.set_servo_pulsewidth(servo_d, 1500)  # Move the servo to its middle position
-        sleep(1)
+def servo1():
+    for a in range(80,100):
+        kit.servo[10].angle = a
+        sleep(0.008)
         
-except KeyboardInterrupt:  # This will catch the interrupt and move to the cleanup code
-    pass
-
-finally:
-    # Disable all servos
-    pi.set_servo_pulsewidth(servo_a, 0)
-    pi.set_servo_pulsewidth(servo_b, 0)
-    pi.set_servo_pulsewidth(servo_c, 0)
-    pi.set_servo_pulsewidth(servo_d, 0)
-    pi.stop()
+    for a in range(100,80,-1):
+        kit.servo[10].angle = a
+        sleep(0.008)
+        
+def servo2():
+    for a in range(80,100):
+        kit.servo[11].angle = a
+        sleep(0.008)
+        
+    for a in range(100,80,-1):
+        kit.servo[11].angle = a
+        sleep(0.008)
+        
+def servo3():
+    for a in range(80,100):
+        kit.servo[14].angle = a
+        sleep(0.008)
+        
+    for a in range(100,80,-1):
+        kit.servo[14].angle = a
+        sleep(0.008)
+        
+def servo4():
+    for a in range(80,100):
+        kit.servo[15].angle = a
+        sleep(0.008)
+        
+    for a in range(100,80,-1):
+        kit.servo[15].angle = a
+        sleep(0.008)
+        
+        
+while True:
+    servo1()
+    servo2()
+    servo3()
+    servo4()
