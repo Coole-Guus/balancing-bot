5bb0e4c5365a45512d4df255da5237c06c3ff5ac
G.J.M. Kleinlein <g.j.m.kleinlein@umail.leidenuniv.nl>
2024-06-20 17:08:41 +0200

    1718896121

--- wheels.py
+++ wheels.py
@@ -13,43 +13,53 @@ from time import sleep
 
 
 # Define the pins
-EN_PIN = 12  # Enable pin
-STEP_PIN = 16  # Step pin
-DIR_PIN = 18  # Direction pin
+R_EN_PIN = 12  # Enable pin
+R_STEP_PIN = 16  # Step pin
+R_DIR_PIN = 18  # Direction pin
 
 
 # Define the pins
-# EN_PIN = 11  # Enable pin
-# STEP_PIN = 13  # Step pin
-# DIR_PIN = 15  # Direction pin
+L_EN_PIN = 11  # Enable pin
+L_STEP_PIN = 13  # Step pin
+L_DIR_PIN = 15  # Direction pin
 
 GPIO.setmode(GPIO.BOARD)
 
 # Create DigitalOutputDevice instances for each pin
-GPIO.setup(EN_PIN, GPIO.OUT)
-GPIO.setup(STEP_PIN, GPIO.OUT)
-GPIO.setup(DIR_PIN, GPIO.OUT)
+GPIO.setup(R_EN_PIN, GPIO.OUT)
+GPIO.setup(R_STEP_PIN, GPIO.OUT)
+GPIO.setup(R_DIR_PIN, GPIO.OUT)
+GPIO.setup(L_EN_PIN, GPIO.OUT)
+GPIO.setup(L_STEP_PIN, GPIO.OUT)
+GPIO.setup(L_DIR_PIN, GPIO.OUT)
+
+
 
 # Function to move the stepper motor
 def move_stepper(steps, direction):
     # print("Moving the stepper motor")
     
     # SET DIRECTION
-    GPIO.output(DIR_PIN, GPIO.HIGH if direction else GPIO.LOW)
+    GPIO.output(R_DIR_PIN, GPIO.HIGH if direction else GPIO.LOW)
+    GPIO.output(L_DIR_PIN, GPIO.HIGH if direction else GPIO.LOW)
     # ENABLE THE MOTOR
-    GPIO.output(EN_PIN, GPIO.LOW)
+    GPIO.output(R_EN_PIN, GPIO.LOW)
+    GPIO.output(L_EN_PIN, GPIO.LOW)
     
     # Move the specified number of steps
     for _ in range(steps):
-        GPIO.output(STEP_PIN, GPIO.HIGH)
-        sleep(0.001)  # Adjust this delay as needed
-        GPIO.output(STEP_PIN, GPIO.LOW)
-        sleep(0.001)  # Adjust this delay as needed
+        GPIO.output(R_STEP_PIN, GPIO.HIGH)
+        GPIO.output(L_STEP_PIN, GPIO.HIGH)
+        sleep(0.0001)  # Adjust this delay as needed
+        GPIO.output(R_STEP_PIN, GPIO.LOW)
+        GPIO.output(L_STEP_PIN, GPIO.LOW)
+        sleep(0.0001)  # Adjust this delay as needed
 
     # # Disable the motor
     # sleep(7)
     # print("Disabling the motor")
-    GPIO.output(EN_PIN, GPIO.HIGH)
+    GPIO.output(R_EN_PIN, GPIO.HIGH)
+    GPIO.output(L_EN_PIN, GPIO.HIGH)
     # sleep(7)
 
 
