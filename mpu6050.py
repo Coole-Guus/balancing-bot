fab869578fe6d5412115b4bf1f34eb6282cd5bd1
G.J.M. Kleinlein <g.j.m.kleinlein@umail.leidenuniv.nl>
2024-06-20 23:43:18 +0200

    oops

--- mpu6050.py
+++ mpu6050.py
@@ -1,9 +1,9 @@
 import mpu6050
 
-mpu6050 = mpu6050.mpu6050(0x68)
+mpu = mpu6050.mpu6050(0x68)
 
 def read_sensor_data():
-    accelerometer_data = mpu6050.get_accel_data()
+    accelerometer_data = mpu.get_accel_data()
     # gyroscope_data = mpu6050.get_gyro_data()
     # temperature = mpu6050.get_temp()
     return accelerometer_data['x']
