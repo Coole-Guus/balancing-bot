677c673ae3ebb36eccc9c10c30096486915a4279
G.J.M. Kleinlein <g.j.m.kleinlein@umail.leidenuniv.nl>
2024-06-20 16:24:21 +0200

    1718893461

--- wheels.py
+++ wheels.py
@@ -48,7 +48,7 @@ def move_stepper(steps, direction):
     en.on()
 
 while True:
-    move_stepper(2000, 0)  # Move 200 steps in one direction
+    move_stepper(2000, 1)  # Move 200 steps in one direction
 
 # Move the stepper motor
 # move_stepper(200, 1)  # Move 200 steps in one direction
