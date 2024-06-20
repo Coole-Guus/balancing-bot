43b200fe6d9ddb792a1a06b0b31c0cedcdd02a3a
G.J.M. Kleinlein <g.j.m.kleinlein@umail.leidenuniv.nl>
2024-06-20 23:48:39 +0200

    oops

--- mpu6050.py
+++ mpu6050.py
@@ -1,9 +1,12 @@
 import adafruit_mpu6050
+import board
 
-mpu = adafruit_mpu6050.MPU6050(0x68)
+i2c = board.I2C()
+mpu = adafruit_mpu6050.MPU6050(i2c)
 
 def read_sensor_data():
-    accelerometer_data = mpu.get_accel_data()
+    # accelerometer_data = mpu.acceleration
     # gyroscope_data = mpu6050.get_gyro_data()
     # temperature = mpu6050.get_temp()
-    return accelerometer_data['x']
+    # return accelerometer_data['x']
+    return mpu.acceleration
