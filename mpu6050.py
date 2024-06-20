d44a8f779ef2b06859997adcbd31d4946234f561
G.J.M. Kleinlein <g.j.m.kleinlein@umail.leidenuniv.nl>
2024-06-21 00:31:38 +0200

    sensor data now

--- mpu6050.py
+++ mpu6050.py
@@ -1,5 +1,6 @@
 import adafruit_mpu6050
 import board
+import math
 
 i2c = board.I2C()
 mpu = adafruit_mpu6050.MPU6050(i2c)
@@ -9,3 +10,9 @@ def read_accel():
 
 def read_gyro():
     return mpu.gyro
+
+def getAngle():
+    acceleration = mpu.acceleration
+    accel_x, accel_y, accel_z = acceleration
+    tilt_angle_x = math.atan2(accel_y, math.sqrt(accel_x**2 + accel_z**2))
+    tilt_angle_x = math.degrees(tilt_angle_x)
--- robot.py
+++ robot.py
@@ -26,42 +26,44 @@ def main():
     legs.changeHeight(110, 110)
     wheels.enable_motors()
     
-    accel_data = mpu6050.read_accel()
-    gyro_data = mpu6050.read_gyro()
+    # accel_data = mpu6050.read_accel()
+    # gyro_data = mpu6050.read_gyro()
         
-    aTempX = accel_data[0]
-    aTempY = accel_data[1]
-    aTempZ = accel_data[2]
-    gTempX = gyro_data[0]
+    # aTempX = accel_data[0]
+    # aTempY = accel_data[1]
+    # aTempZ = accel_data[2]
+    # gTempX = gyro_data[0]
 
-    last_x = x_rotation(aTempX, aTempY, aTempZ)
+    # last_x = x_rotation(aTempX, aTempY, aTempZ)
 
-    gyro_offset_x = gTempX
+    # gyro_offset_x = gTempX
     try:
         while True:
-            accel_data = mpu6050.read_accel()
-            gyro_data = mpu6050.read_gyro()
+            # accel_data = mpu6050.read_accel()
+            # gyro_data = mpu6050.read_gyro()
 
-            accelX = accel_data[0]
-            accelY = accel_data[1]
-            accelZ = accel_data[2]
+            # accelX = accel_data[0]
+            # accelY = accel_data[1]
+            # accelZ = accel_data[2]
 
-            gyroX = gyro_data[0]
-            gyroX -= gyro_offset_x
+            # gyroX = gyro_data[0]
+            # gyroX -= gyro_offset_x
 
-            gyro_x_delta = (gyroX * timeSlice)
+            # gyro_x_delta = (gyroX * timeSlice)
 
-            rotation_x = x_rotation(accelX, accelY, accelZ)
+            # rotation_x = x_rotation(accelX, accelY, accelZ)
             
-            #Complementary Filter
-            last_x = K * (last_x + gyro_x_delta) + (K1 * rotation_x)
-            print(f"X: {last_x}")
+            # #Complementary Filter
+            # last_x = K * (last_x + gyro_x_delta) + (K1 * rotation_x)
+            # print(f"X: {last_x}")
             # sleep(timeSlice)
-            interpXAccel = interp(last_x, [-5, 5], [-100, 100])
-            if interpXAccel < 0:
-                wheels.move_stepper(timeSlice, abs(interpXAccel), "forward")
-            else:
-                wheels.move_stepper(timeSlice, abs(interpXAccel), "backward")
+            print(f"X: {mpu6050.getAngle()}")
+            sleep(0.2)
+            # interpXAccel = interp(last_x, [-5, 5], [-100, 100])
+            # if interpXAccel < 0:
+            #     wheels.move_stepper(timeSlice, abs(interpXAccel), "forward")
+            # else:
+            #     wheels.move_stepper(timeSlice, abs(interpXAccel), "backward")
             
     except KeyboardInterrupt:
         wheels.disable_motors()
