bcb23e58c75918e3bb213175c76fcc51dbc39cc5
G.J.M. Kleinlein <g.j.m.kleinlein@umail.leidenuniv.nl>
2024-06-21 00:24:42 +0200

    sensor data now

--- wheels.py
+++ wheels.py
@@ -54,7 +54,7 @@ def move_stepper(timeSlice, speed, direction):
         sleep(timeSlice)
         return
     
-    speed = speed / timeSlice*200
+    speed = speed / 5
     
     totalSteps = speed / timeSlice
     sleepTime = (timeSlice / totalSteps) / 2
