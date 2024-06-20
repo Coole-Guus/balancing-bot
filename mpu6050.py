c1f40d71c38ac2b535de255259f5c9682cc9228e
G.J.M. Kleinlein <g.j.m.kleinlein@umail.leidenuniv.nl>
2024-06-21 00:41:39 +0200

    sensor data now

--- mpu6050.py
+++ mpu6050.py
@@ -24,28 +24,5 @@ def correct_for_gravity(acc):
     return acc / g
 
 def getAngle():
-    # Read gyroscope data
-    gyro = mpu.gyro
-    
-    # Integrate gyroscope data to get angular displacement
-    accumulated_angle_x += gyro['x']
-    accumulated_angle_y += gyro['y']
-    accumulated_angle_z += gyro['z']
-    
-    # Correct for drift using accelerometer data
     acc = mpu.acceleration
-    corrected_acc_x = correct_for_gravity(acc['x'])
-    corrected_acc_y = correct_for_gravity(acc['y'])
-    corrected_acc_z = correct_for_gravity(acc['z'])
-    
-    # Calculate gravity vector
-    gravity_vector = [corrected_acc_x, corrected_acc_y, corrected_acc_z]
-    
-    # Subtract gravity vector from integrated gyroscope data
-    corrected_angle_x = accumulated_angle_x - gravity_vector[0]
-    corrected_angle_y = accumulated_angle_y - gravity_vector[1]
-    corrected_angle_z = accumulated_angle_z - gravity_vector[2]
-    
-    # Print corrected angles in degrees
-    print(f"Corrected Angles: X={corrected_angle_x}, Y={corrected_angle_y}, Z={corrected_angle_z} degrees")
-    
+    return math.degrees(math.atan2(acc['x'], acc['z']))
--- robot.py
+++ robot.py
@@ -6,65 +6,15 @@ import wheels
 import legs
 import mpu6050
 
-K = 0.98
-K1 = 1 - K
-
-timeSlice = 0.02
-
-def distance(a, b):
-    return math.sqrt((a*a) + (b*b))
-
-def y_rotation(x, y, z):
-    radians = math.atan2(x, distance(y, z))
-    return -math.degrees(radians)
-
-def x_rotation(x, y, z):
-    radians = math.atan2(y, distance(x, z))
-    return math.degrees(radians)
-    
 def main():
     legs.changeHeight(110, 110)
     wheels.enable_motors()
-    
-    # accel_data = mpu6050.read_accel()
-    # gyro_data = mpu6050.read_gyro()
-        
-    # aTempX = accel_data[0]
-    # aTempY = accel_data[1]
-    # aTempZ = accel_data[2]
-    # gTempX = gyro_data[0]
 
-    # last_x = x_rotation(aTempX, aTempY, aTempZ)
-
-    # gyro_offset_x = gTempX
     try:
         while True:
-            # accel_data = mpu6050.read_accel()
-            # gyro_data = mpu6050.read_gyro()
-
-            # accelX = accel_data[0]
-            # accelY = accel_data[1]
-            # accelZ = accel_data[2]
-
-            # gyroX = gyro_data[0]
-            # gyroX -= gyro_offset_x
-
-            # gyro_x_delta = (gyroX * timeSlice)
-
-            # rotation_x = x_rotation(accelX, accelY, accelZ)
-            
-            # #Complementary Filter
-            # last_x = K * (last_x + gyro_x_delta) + (K1 * rotation_x)
-            # print(f"X: {last_x}")
-            # sleep(timeSlice)
             print(f"X: {mpu6050.getAngle()}")
             sleep(0.5)
-            # interpXAccel = interp(last_x, [-5, 5], [-100, 100])
-            # if interpXAccel < 0:
-            #     wheels.move_stepper(timeSlice, abs(interpXAccel), "forward")
-            # else:
-            #     wheels.move_stepper(timeSlice, abs(interpXAccel), "backward")
-            
+
     except KeyboardInterrupt:
         wheels.disable_motors()
 
