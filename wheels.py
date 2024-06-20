bdfbba0c066b00ce6e46986914b971bf2c9a7e32
G.J.M. Kleinlein <g.j.m.kleinlein@umail.leidenuniv.nl>
2024-06-20 13:44:20 +0200

    chaing steppers

--- wheels.py
+++ wheels.py
@@ -1,7 +1,6 @@
-import time
-from TMC_2209.TMC_2209_StepperDriver import *
-from TMC_2209._TMC_2209_GPIO_board import Board
-    
+from gpiozero import DigitalOutputDevice
+from time import sleep
+
 # stepper left pinout:
 # en = 11
 # step = 13
@@ -12,17 +11,10 @@ from TMC_2209._TMC_2209_GPIO_board import Board
 # step = 16
 # dir = 18
 
-from gpiozero import DigitalOutputDevice
-from time import sleep
-
 # Define the pins
-EN_PIN = 12  # Enable pin
-STEP_PIN = 16  # Step pin
-DIR_PIN = 18  # Direction pin
-
-# EN_PIN = 11  # Enable pin
-# STEP_PIN = 13  # Step pin
-# DIR_PIN = 15  # Direction pin
+EN_PIN = 11  # Enable pin
+STEP_PIN = 13  # Step pin
+DIR_PIN = 15  # Direction pin
 
 # Create DigitalOutputDevice instances for each pin
 en = DigitalOutputDevice(EN_PIN)
@@ -30,29 +22,24 @@ step = DigitalOutputDevice(STEP_PIN)
 dir = DigitalOutputDevice(DIR_PIN)
 
 # Function to move the stepper motor
-# def move_stepper(steps, direction):
-#     # Set direction
-#     dir.value = direction
+def move_stepper(steps, direction):
+    # Set direction
+    dir.value = direction
 
-#     # Enable the motor
-#     en.off()
+    # Enable the motor
+    en.off()
 
-#     # Move the specified number of steps
-#     for _ in range(steps):
-#         step.on()
-#         sleep(0.1)  # Adjust this delay as needed
-#         step.off()
-#         sleep(0.1)  # Adjust this delay as needed
+    # Move the specified number of steps
+    for _ in range(steps):
+        step.on()
+        sleep(0.001)  # Adjust this delay as needed
+        step.off()
+        sleep(0.001)  # Adjust this delay as needed
 
-#     # Disable the motor
-#     en.on()
+    # Disable the motor
+    en.on()
 
-
-en.on()
-
-# while True:
-    # move_stepper(200, 1)  # Move 200 steps in one direction 
 # Move the stepper motor
-# move_stepper(200, 1)  # Move 200 steps in one direction
-# sleep(1)  # Wait for a second
-# move_stepper(200, 0)  # Move 200 steps in the other direction
+move_stepper(200, 1)  # Move 200 steps in one direction
+sleep(1)  # Wait for a second
+move_stepper(200, 0)  # Move 200 steps in the other direction
