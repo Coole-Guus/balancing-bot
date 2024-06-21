43fb344aacdcad77986367fb2fd8cd19776628f0
G.J.M. Kleinlein <g.j.m.kleinlein@umail.leidenuniv.nl>
2024-06-21 13:26:02 +0200

    testing something

--- wheels.py
+++ wheels.py
@@ -93,7 +93,7 @@ def move_stepper(speed):
         sleep(iterationTime)
         return
     
-    speed = speed / 10
+    speed = speed / 100
     
     totalSteps = speed / iterationTime
     sleepTime = (iterationTime / totalSteps) / 2
